dcdb3b2ac6ac5a5aabe74a90bafb63bdc928e6fe
곽승민 <ekekektmdals@gmail.com>
2026-01-04 19:09:17 +0900

    메인 및 로고 정리

--- js/common.js
+++ js/common.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-
+    const introMain = document.querySelector(".intro");
+    const introStart = document.querySelector(".intro_btn");
+    
+    introStart.addEventListener("click", ()=>{
+        introMain.style.display = "none";
+    });
 });
