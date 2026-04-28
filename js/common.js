2f854f6cd879de15a39f268f435e9d5f5cece656
곽승민 <ekekektmdals@gmail.com>
2026-04-28 14:54:44 +0900

    포트폴리오 완성

--- js/common.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    const introMain = document.querySelector(".intro");
-    const introStart = document.querySelector(".intro_btn");
-    
-    introStart.addEventListener("click", ()=>{
-        introMain.style.display = "none";
-    });
-});
